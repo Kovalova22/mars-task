14534601a9e7fcf2c9ee80d70f23038c8af54f3f
Iryna Kovalova <ikovalova333@gmail.com>
2021-04-25 21:17:07 +0300

    add filters

--- src/components/photos.tsx
+++ src/components/photos.tsx
@@ -1,30 +1,64 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 
 import '../App.scss';
 import { Link } from 'react-router-dom';
+import { Input } from 'reactstrap';
 
 const apiKey = process.env.REACT_APP_NASA_KEY;
 
 export default function DisplayPictures() {
-  const perPage = 3;
+  const perPage = 1;
   const [totalPages, setTotalPages] = useState(1);
   const [page, setPage] = useState(1);
 
   const [photoData, setPhotoData] = useState<any>([]);
 
-  const sol = 1000;
-  const cam = 'FHAZ';
-  const rover = 'Curiosity';
+  const [state, setState] = useState<any>({
+    sol: '1000',
+    cam: 'FHAZ',
+    rover: 'Curiosity',
+  });
+
+  const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    const value = evt.target.value;
+
+    setState({
+      ...state,
+      [evt.target.name]: value,
+    });
+  };
+
+  const handleSubmit = () => {
+    const sol = state.sol;
+    const cam = state.cam;
+    const rover = state.rover;
+    const data = {
+      sol,
+      cam,
+      rover,
+    };
+    console.log(data);
+
+    return data;
+  };
+  const sol = handleSubmit().sol;
+  const cam = handleSubmit().cam;
+  const rover = handleSubmit().rover;
+
+  const link = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&camera=${cam}&rover=${rover}&per_page=${perPage}&page=${page}&api_key=${apiKey}`;
 
   useEffect(() => {
     const nasaResponse = () => {
-      fetch(
-        `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&camera=${cam}&rover=${rover}&per_page=${perPage}&page=${page}&api_key=${apiKey}`
-      )
+      fetch(link)
         .then((res) => res.json())
         .then((res) => {
           setTotalPages(1000);
           setPhotoData([...photoData, ...res.photos]);
+          if (!res.photos[0]) {
+            alert('No more pictures with such characteristics!');
+            window.location.reload();
+          }
           console.log(res);
         });
     };
@@ -37,6 +71,46 @@ export default function DisplayPictures() {
         <Link className="go-back-link" to="/">
           Go back
         </Link>
+        <div className="options">
+          <Input
+            className="select-input"
+            type="select"
+            name="sol"
+            value={state.sol}
+            onChange={handleChange}
+          >
+            <option value="1000">1000</option>
+            <option value="900">900</option>
+          </Input>
+          <Input
+            className="select-input"
+            type="select"
+            name="cam"
+            value={state.cam}
+            onChange={handleChange}
+          >
+            <option value="FHAZ">FHAZ</option>
+            <option value="RHAZ">RHAZ</option>
+            <option value="MAST">MAST</option>
+            <option value="CHEMCAM">CHEMCAM</option>
+            <option value="MAHLI">MAHLI</option>
+            <option value="MARDI">MARDI</option>
+            <option value="NAVCAM">NAVCAM</option>
+            <option value="PANCAM">PANCAM</option>
+            <option value="MINITES">MINITES</option>
+          </Input>
+          <Input
+            className="select-input"
+            type="select"
+            name="rover"
+            value={state.rover}
+            onChange={handleChange}
+          >
+            <option value="Curiosity">Curiosity</option>
+            <option value="Opportunity">Opportunity</option>
+            <option value="Spirit">Spirit</option>
+          </Input>
+        </div>
         <div className="container">
           <div className="location">
             {photoData.map((x, y) => (
@@ -46,7 +120,13 @@ export default function DisplayPictures() {
             ))}
           </div>
           {totalPages !== page && (
-            <button className="load-button" onClick={() => setPage(page + 1)}>
+            <button
+              className="load-button"
+              onClick={() => {
+                setPage(page + 1);
+                handleSubmit();
+              }}
+            >
               Load More
             </button>
           )}
